cd9fe7340b2e1a7ed3f6c0ee0768f0a4213be44f
Shushpancheak <Makcieml@gmail.com>
2020-10-11 04:23:52 +0300

    standalone rust app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![mp_main]
+
+use core::panic::PanicInfo
+
+
+/**
+ * \brief The entry point of the boot.
+ */
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+
+/** 
+ * \brief The function called on panic.
+ */
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
